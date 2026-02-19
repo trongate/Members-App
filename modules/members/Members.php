f06d59a55814d63243e80466ff4a8a85634c9e5d
David Connelly <davidc.webguy@gmail.com>
2026-02-19 13:46:54 +0000

    Changed login destination to members/your_account.

--- modules/members/Members.php
+++ modules/members/Members.php
@@ -367,7 +367,7 @@ class Members extends Trongate {
 
         // Execute the login process
         $member_obj = $this->model->log_user_in($member_obj);
-        $member_obj->target_url = ($member_obj->password === '') ? 'members/update_password' : 'dashboard';
+        $member_obj->target_url = ($member_obj->password === '') ? 'members/update_password' : 'members/your_account';
         return $member_obj;
     }
 
